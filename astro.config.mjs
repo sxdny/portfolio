194ebda64205a6371261d1b137f3016a5e2613f5
Sidney Silva Braz de Oliveira <silvasidney721@gmail.com>
2024-03-30 20:23:19 +0100

    feat: new Home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://portfolio.sxdny.dev",
+  site: 'https://portfolio.sxdny.dev',
 });
