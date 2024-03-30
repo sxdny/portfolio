607ee70e890bb0156666c49b9bd562918a8d06d5
sxdny <67187745+sxdny@users.noreply.github.com>
2024-03-30 20:59:59 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://portfolio.sxdny.dev',
+	site: 'https://portfolio.sxdny.dev',
+	integrations: [mdx(), sitemap()],
 });
