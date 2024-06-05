8fa16d8b810563438f325275bb924f94880bcbb1
sxdny <67187745+sxdny@users.noreply.github.com>
2024-06-05 14:43:04 +0200

    feat: new portfolio page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://portfolio.sxdny.dev',
+  site: 'https://portfolio.sxdny.dev',
+  integrations: [tailwind()]
 });
